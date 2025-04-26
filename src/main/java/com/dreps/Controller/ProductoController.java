d8784bb21dfc68aeda3499b278b01f9396c025bc
AstrodevJV <soalejandro@protonmail.com>
2025-04-26 11:58:52 -0500

    Eliminar librerias no usadas

--- src/main/java/com/dreps/Controller/ProductoController.java
+++ src/main/java/com/dreps/Controller/ProductoController.java
@@ -5,8 +5,6 @@ import com.dreps.Model.ProductoModel;
 import com.dreps.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
