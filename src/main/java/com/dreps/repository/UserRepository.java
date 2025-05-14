dc6f3890d633d086f0297ee8d19efaf50895e6a7
Astro <soalejandro@protonmail.com>
2025-05-14 03:57:49 +0000

    feat(repository): add method to find user by email

--- src/main/java/com/dreps/repository/UserRepository.java
+++ src/main/java/com/dreps/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.dreps.Model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
+    Optional<UserModel> findByEmail(String email);
 
 }
