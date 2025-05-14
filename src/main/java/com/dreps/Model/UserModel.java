d3062e5364c1b51ea16a59f082158d79c1f81547
Astro <soalejandro@protonmail.com>
2025-05-14 03:37:36 +0000

    feat(model): add new class for UserModel

---
+++ src/main/java/com/dreps/Model/UserModel.java
@@ -0,0 +1,34 @@
+package com.dreps.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "usuarios")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre",nullable = false)
+    private String nombre;
+
+    @Column(name = "email",nullable = false)
+    private String email;
+
+    @Column(name = "contrasena",nullable = false)
+    private String contrasena;
+
+    @Column(name = "rol",nullable = true,length = 20)
+    private String rol;
+}
